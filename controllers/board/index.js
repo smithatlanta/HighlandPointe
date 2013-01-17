54d3af4e976f8f396501e9928eeb02e771c31fc8
Michael Smith <smithatlanta@gmail.com>
2013-01-17 11:43:03 -0500

    made items relative path

--- controllers/board/index.js
+++ controllers/board/index.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
 exports.index = function(req, res){
-	fs.readdir('public/minutes', function(err, data){
+	fs.readdir('./public/minutes', function(err, data){
 		if (err)
 			console.log(err);
-		
+		data.sort();
+		data.reverse();
 		res.render('board/index', { title: 'Highland Pointe Online', "files": data });
 	});
 };
--- controllers/newsletter/index.js
+++ controllers/newsletter/index.js
@@ -1,10 +1,11 @@
 var fs = require('fs');
 
 exports.index = function(req, res){
-	fs.readdir('public/issues', function(err, data){
+	fs.readdir('./public/issues', function(err, data){
 		if (err)
 			console.log(err);
-
+		data.sort();
+		data.reverse();
 		res.render('newsletter/index', { title: 'Highland Pointe Online', "files": data });
 	});
 };
